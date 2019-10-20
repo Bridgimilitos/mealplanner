272c8e62d9c4be9fce4dc8ea4a347a2ab1a40dd5
Bridgimilitos <jbridges@f2s.com>
2019-10-20 12:06:16 +0100

    Hello world flask app

---
+++ db/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'flask',
+    'flask-sqlalchemy',
+    'pyodbc',
+]
+
+setup(
+    name='mealplanner_db',
+    version='0.0',
+    description='db interface to sql server',
+    author='James Bridges',
+    author_email='jbridges@f2s.com',
+    keywords='web flask',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires
+)
